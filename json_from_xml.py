f0ba7b6541ad6dadaa3c4e4219fc24e538a7b9c3
jide <jride@wisc.edu>
2021-12-01 17:05:40 -0600

    Cleanup some obsolete code.

--- json_from_xml.py
+++ json_from_xml.py
@@ -30,22 +30,6 @@ Arguments: \n
     main(input_file)
 
 
-# def to_json(node: Node, indent: int = 1) -> str:
-#     """
-#     Converts a Metapype model instance to JSON
-#
-#     Args:
-#         indent:
-#         node: node of the model instance
-#
-#     Returns:
-#         str: JSON of Metapype model instance
-#
-#     """
-#     j = _serialize(node)
-#     return json.dumps(j, indent=indent)
-
-
 def _json_from_xml(filename):
     xml = None
     if not filename.lower().endswith('.xml'):
@@ -61,11 +45,7 @@ def _json_from_xml(filename):
     if not isinstance(eml, Node):
         print('Metapype failed to validate the XML input. Exiting.')
         exit(0)
-    # return mp_io.to_json(eml)
     return metapype_io.to_json(eml)
-    # _json = metapype_io.to_json(eml)
-    # parsed = json.loads(_json)
-    # return json.dumps(parsed, indent=1, sort_keys=False)
 
 
 def main(input_filename: str):
@@ -73,9 +53,8 @@ def main(input_filename: str):
     output_filename = input_filename[:-4] + '.json'
     with open(output_filename, 'w') as output_file:
         output_file.write(_json)
-    print('Done. Exiting.')
+    print('Success')
 
 
 if __name__ == '__main__':
-    # main('/Users/jide/Downloads/NTL_people_sites_template.xml')
     json_from_xml()
